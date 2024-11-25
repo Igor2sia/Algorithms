9be9b8bdcc001b62007f885d2dc5581ca6a701c4
4pok <91655233+Igor2sia@users.noreply.github.com>
2024-11-25 14:23:49 +0300

    Delete src/MinionsComparator.java

--- src/MinionsComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class MinionsComparator implements Comparator<Minions> {
-
-    @Override
-    public int compare(Minions o1, Minions o2) {
-        return o1.getName().compareTo(o2.getName());
-    }
-}
